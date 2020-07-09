a41a076a848b88789a35d163e8d00754131b6426
davidko604 <davidko604@gmail.com>
2020-07-08 17:59:22 -0700

    Add clucks new page

--- routes/clucks.js
+++ routes/clucks.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
+// const knex = require('../db/client')
+const cookieParser = require('cookie-parser')
+
+router.use(cookieParser())
 
 //  GET new cluck page
-router.get('/newCluck', (_req, _res) => {
-  _res.render('newClk')
+router.get('/new', (req, res) => {
+  const username = req.cookies.username
+  res.render('clucks/new', { username })
 })
+
+
 
 module.exports = router
